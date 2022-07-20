dd46a8edbed8e288e0091d02ae31c64d246aa4ca
Melody Lee <melody.lee357@gmail.com>
2022-07-20 13:58:03 -0400

    gradient descent function -- has errors

---
+++ gradient_descent.py
@@ -0,0 +1,60 @@
+# arbitrary theta0 and theta1 starting values
+theta0 = 0 # constant term
+theta1 = 0 
+
+a = 0.1 # step 
+
+# create a sample dataset
+def get_dataset():
+    # generate dataset
+    return [1, 2, 3], [1, 2, 3]
+
+# calculate the sum of the difference
+def sum_of_differences(h, x, y, notConstant = False):
+    sum = 0
+    # sum the difference between the predicted and actual value
+    for i in range(len(x)):
+        temp = h(x[i]) - y[i]
+        if notConstant:
+            sum += temp * x[i]
+        else:
+            sum += temp
+    return sum
+
+# h function
+# lambda x: theta0 + theta1*x
+
+# get the dataset 
+x, y = get_dataset()
+
+if len(x) != len(y):
+    raise Exception("x and y dataset lengths do not match")
+
+m = len(x)
+
+temp_theta0 = 0
+temp_theta1 = 0
+
+h = lambda x: theta0 + theta1*x
+
+min_error = sum_of_differences(h, x, y) ** 2
+curr_error = sum_of_differences(h, x, y) ** 2
+
+while curr_error <= min_error:
+    
+    min_error = curr_error
+    # simultaneously update
+    temp_theta0 = theta0 - (1 / float(m)) * a * sum_of_differences(h, x, y)
+    temp_theta1 = theta1 - (1 / float(m)) * a * sum_of_differences(h, x, y, notConstant = True)
+    theta0 = temp_theta0
+    theta1 = temp_theta1
+    print(theta0)
+    print(theta1)
+
+    h = lambda x: theta0 + theta1*x
+
+    curr_error = sum_of_differences(h, x, y) ** 2
+
+print("Theta 0 = " + str(theta0))
+print("Theta 1 = " + str(theta1))
+
