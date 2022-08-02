adcb9cc345eced1f05a40f446e69c614482da1cd
Melody Lee <melody.lee357@gmail.com>
2022-08-02 01:59:52 -0400

    updates

--- federated_sgd.py
+++ federated_sgd.py
@@ -6,23 +6,19 @@ def aggregateMatrix(LG_set = np.array([])):
     # if nothing is passed in
     if LG_set.size == 0:
         return None
-    V = np.zeros(shape = (LG[0].size, LG[0][0].size))
 
-    for LG in LG_set:
-        for i in LG.size:
-            for j in LG[0].size:
-                V[i, j] += LG[i, j]
 
-    # UNCERTAIN: get the average? (aggregate)
-    for i in V.size:
-        for j in V[0].size:
-            V[i, j] = V[i, j] / LG_set.size
-
-    return V
+    return
 
 def updateV(LG, V, alpha=0.1, beta=0.01):
-    if V.size == 0:
-        return None
-    
-    for i in V.size:
-        for j in V[0].size:
+    if LG.size == 0:
+        return 
+    elif LG[0].size == 0:
+        return
+    for i in LG.size:
+        for j in LG[0].size:
+
+    return
+
+
+
--- sgd.py
+++ sgd.py
@@ -14,22 +14,16 @@ from linear_tools import MSE, RelError
 # create U0, V0, where U = users, V = ratings
 # note V is downloaded at the start
 def calculate_local_sgd(data, V, maxiter=100, alpha=0.1, beta=0.01, k=10, created_sparse_data = False, actual_data = []): # add sparse versus 
-    # if created_sparse_data and not data:
-    #     raise Exception("Please include the original dataset")
-    # elif created_sparse_data:
-    #     # create error vector
-    #     errorVec = np.zeros((2, maxiter), dtype='float')
-    #     RelErr_prev = 1
     
     # conduct preliminary setup
-    N = data.length # matrix size
+    N = data.size # matrix size
     U = np.random.normal(size=(N, k), scale=1/k)
-
+    LG = np.random.normal(size=(N, k), scale=1/k)
     # remove this and replace with download V
     # V = np.random.normal(size=(N, k), scale=1/k) 
     
     # for every iteration in amount specified
-    for iteration in maxiter:
+    for iteration in range(maxiter):
         # randomize data, select 10% of items
         np.random.shuffle(data)
         selected_updates = data[1:(int)(np.floor(0.1*len(data)))]
@@ -41,10 +35,13 @@ def calculate_local_sgd(data, V, maxiter=100, alpha=0.1, beta=0.01, k=10, create
             # update U, equation <1> 
             U[i, :] -= alpha * (error*V[j, :] + beta*U[i, :])
             
-            # update V
-            V[j, :] -= alpha * (error*U_temp + beta*V[j, :]) # UNCERTAIN: this update function doesn't reflect the mathematics
-    return V
+            # computer the local gradient
+            LG[j, :] = alpha * (error*U[i,:]) # UNCERTAIN: this update function doesn't reflect the mathematics
 
+            # calculate local gradient
+            
+
+    return LG
 
 
     
